4436cc84833f2c40671504ddb3a8b1b0679a9044
thmq169 <88388218+thmq169@users.noreply.github.com>
2024-02-19 11:25:59 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,6 +15,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.send("Hi!, welcome to kanban api"));
+
+app.use((req, res, next) => {
+  // const allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
+  // const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  //      res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  return next();
+});
 
 app.use("/api/v1", indexRouter);
 
