e03b4538675eebe336c199c1f18d4dd8d5506bd0
thmq169 <88388218+thmq169@users.noreply.github.com>
2024-02-19 12:03:56 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,12 +15,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(function(req, res, next) {
-            res.header("Access-Control-Allow-Origin", "http://localhost:5173/"); // update to match the domain you will make the request from
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-            next();
-          });
-        app.get("/", (req, res) => res.send("Hi!, welcome to kanban api"));
 
 app.use("/api/v1", indexRouter);
 
